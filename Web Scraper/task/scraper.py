131f6195b6be5580a7b2ce0dc3fcd2fc8e172240
jairo8925 <csarmiento2333@gmail.com>
2021-06-28 19:33:18 -0700

    added BeautifulSoup to web scraper

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -1,11 +1,25 @@
 import requests
+import json
+
+from bs4 import BeautifulSoup
 
 
-def get_quote(url):
-    r = requests.get(url)
-    quote = r.json().get('content')
-    return quote if quote else "Invalid quote resource!"
+def get_movie(url):
+    r = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
+
+    soup = BeautifulSoup(r.text, "html.parser")
+    movie_info = json.loads("".join(soup.find("script", {"type": "application/ld+json"}).contents))
+    movie = {}
+    if movie_info["@type"] == "Movie" or movie_info["@type"] == "TVSeries":
+        movie["title"] = movie_info["name"]
+        movie["description"] = movie_info["description"]
+        print(movie)
+    else:
+        print("Invalid movie page!")
 
 
 user_input = input()
-print(get_quote(user_input))
+if "https://www.imdb.com/title/" not in user_input:
+    print("Invalid movie page!")
+else:
+    get_movie(user_input)
