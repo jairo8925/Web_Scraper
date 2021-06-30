01b4a9919ce5d8b6a67b3472bcaaa9f1963e0c5f
jairo8925 <csarmiento2333@gmail.com>
2021-06-29 17:50:56 -0700

    cleaned up code

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -1,48 +1,50 @@
 import requests
-import json
-
-from bs4 import BeautifulSoup
-
-
-import requests
 import string
 
 from bs4 import BeautifulSoup
 
+website = "https://www.nature.com/nature/articles"
+
 
 def main():
+
     saved_articles = []
-    links = []
-    r = requests.get("https://www.nature.com/nature/articles")
-    if r.status_code == 200:
-        page_content = r.content
-        soup = BeautifulSoup(page_content, "html.parser")
-        articles = soup.find_all('article')
-        for a in articles:
-            span_tags = a.find_all("span", {"class": "c-meta__type"})
-            for t in span_tags:
-                if t.text == "News":
-                    link = a.find('a')
-                    links.append(link.get('href'))
-        # print(links)
-        for a in links:
-            r = requests.get("https://www.nature.com" + a)
-            soup = BeautifulSoup(r.content, "html.parser")
-            title = soup.find('title').text
-            title = title.translate(str.maketrans('', '', string.punctuation))
-            title = title.replace(' ', '_')
-            body = soup.find('div', {"class": "c-article-body"})
-            body = body.text.strip()
-            # body = body.replace("\n", "")
-            filename = title + ".txt"
-            file = open(filename, "wb")
-            file.write(body.encode('utf-8'))
-            file.close()
-            # print(title)
-            # print(body)
-            saved_articles.append(filename)
+    req = requests.get(website)
+
+    if req.status_code == 200:
+
+        soup = BeautifulSoup(req.content, "html.parser")
+        articles = soup.find_all('article')  # find all articles
+
+        for article in articles:
+            span_tags = article.find_all("span", {"class": "c-meta__type"})
+            for span in span_tags:
+                # check if article type is "News"
+                if span.text == "News":
+
+                    # get link of article
+                    link = article.find('a').get('href')
+                    r = requests.get("https://www.nature.com" + link)
+                    soup = BeautifulSoup(r.content, "html.parser")
+
+                    # format title
+                    title = soup.find('title').text  # get title of article
+                    title = title.translate(str.maketrans('', '', string.punctuation))  # remove punctuation
+                    title = title.replace(' ', '_')  # replace whitespace w/ underscore
+
+                    # extract news body
+                    body = soup.find('div', {"class": "c-article-body"})
+                    body = body.text.strip()
+
+                    # save article to .txt file
+                    filename = title + '.txt'
+                    file = open(filename, 'wb')
+                    file.write(body.encode('utf-8'))
+                    saved_articles.append(filename)
+                    file.close()
+
     else:
-        print("The URL returned " + str(r.status_code) + "!")
+        print("The URL returned " + str(req.status_code) + "!")
 
     print(saved_articles)
 
