da3052d163579ac8da4f06556aade052a4aba2cf
jairo8925 <csarmiento2333@gmail.com>
2021-06-29 19:41:10 -0700

    allowed scraping of other article types and getting more than one page

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import string
+import os
 
 from bs4 import BeautifulSoup
 
@@ -9,44 +10,56 @@ website = "https://www.nature.com/nature/articles"
 def main():
 
     saved_articles = []
-    req = requests.get(website)
+    pages = int(input())
+    article_type = input()
+    for i in range(1, pages + 1):
+        req = requests.get(website, params={'page': i})
 
-    if req.status_code == 200:
+        dir_name = 'Page_' + str(i)
+        if not os.path.exists(dir_name):
+            os.mkdir(dir_name)
 
-        soup = BeautifulSoup(req.content, "html.parser")
-        articles = soup.find_all('article')  # find all articles
+        if req.status_code == 200:
 
-        for article in articles:
-            span_tags = article.find_all("span", {"class": "c-meta__type"})
-            for span in span_tags:
-                # check if article type is "News"
-                if span.text == "News":
+            soup = BeautifulSoup(req.content, "html.parser")
+            articles = soup.find_all('article')  # find all articles
 
-                    # get link of article
-                    link = article.find('a').get('href')
-                    r = requests.get("https://www.nature.com" + link)
-                    soup = BeautifulSoup(r.content, "html.parser")
+            for article in articles:
+                span_tags = article.find_all("span", {"class": "c-meta__type"})
+                for span in span_tags:
+                    # check article type
+                    if span.text == article_type:
 
-                    # format title
-                    title = soup.find('title').text  # get title of article
-                    title = title.translate(str.maketrans('', '', string.punctuation))  # remove punctuation
-                    title = title.replace(' ', '_')  # replace whitespace w/ underscore
+                        # get link of article
+                        link = article.find('a').get('href')
+                        r = requests.get("https://www.nature.com" + link)
+                        soup = BeautifulSoup(r.content, "html.parser")
 
-                    # extract news body
-                    body = soup.find('div', {"class": "c-article-body"})
-                    body = body.text.strip()
+                        # format title
+                        title = soup.find('meta', {"property": "og:title"}).get('content')  # get title of article
+                        title = title.translate(str.maketrans('', '', string.punctuation))  # replace punctuation
+                        title = title.replace(' ', '_')  # remove whitespace
 
-                    # save article to .txt file
-                    filename = title + '.txt'
-                    file = open(filename, 'wb')
-                    file.write(body.encode('utf-8'))
-                    saved_articles.append(filename)
-                    file.close()
+                        # extract news body
+                        body = soup.find('div', {"class": "c-article-body"})
+                        if body is None:
+                            body = soup.find('div', {"class": "article-item__body"})
+                        if body is None:
+                            body = soup.find('article')
+                        body = body.text.strip()
 
-    else:
-        print("The URL returned " + str(req.status_code) + "!")
+                        # save article to .txt file
+                        txt_file = title + '.txt'
+                        filename = os.path.join(dir_name, txt_file)
+                        file = open(filename, 'wb')
+                        file.write(body.encode('utf-8'))
+                        saved_articles.append(txt_file)
+                        file.close()
 
-    print(saved_articles)
+        else:
+            print("The URL returned " + str(req.status_code) + "!")
+
+    print("Saved all articles.")
 
 
 if __name__ == "__main__":
